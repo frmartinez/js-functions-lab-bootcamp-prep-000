41225bcd7d52cfdd15cffb317aec2715b349e540
Fernando Martinez <martinezfer@gmail.com>
2019-01-17 20:01:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // write your code below!
-function happyHolidays(string) {
+function happyHolidays() {
   return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy holidays!, ${name}'
 }
